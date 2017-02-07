2ae00ce8af5bce92da7e28e79c7c8088ad20de01
nik2079 <nik2079@narod.ru>
2017-02-07 20:24:48 +0700

    коммит

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.ru/admin/');
+define('HTTP_CATALOG', 'http://opencart.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.ru/admin/');
+define('HTTPS_CATALOG', 'http://opencart.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\openserver\OpenServer\domains\opencart.ru/admin/');
+define('DIR_SYSTEM', 'D:\openserver\OpenServer\domains\opencart.ru/system/');
+define('DIR_DATABASE', 'D:\openserver\OpenServer\domains\opencart.ru/system/database/');
+define('DIR_LANGUAGE', 'D:\openserver\OpenServer\domains\opencart.ru/admin/language/');
+define('DIR_TEMPLATE', 'D:\openserver\OpenServer\domains\opencart.ru/admin/view/template/');
+define('DIR_CONFIG', 'D:\openserver\OpenServer\domains\opencart.ru/system/config/');
+define('DIR_IMAGE', 'D:\openserver\OpenServer\domains\opencart.ru/image/');
+define('DIR_CACHE', 'D:\openserver\OpenServer\domains\opencart.ru/system/cache/');
+define('DIR_DOWNLOAD', 'D:\openserver\OpenServer\domains\opencart.ru/download/');
+define('DIR_LOGS', 'D:\openserver\OpenServer\domains\opencart.ru/system/logs/');
+define('DIR_CATALOG', 'D:\openserver\OpenServer\domains\opencart.ru/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.ru/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\openserver\OpenServer\domains\opencart.ru/catalog/');
+define('DIR_SYSTEM', 'D:\openserver\OpenServer\domains\opencart.ru/system/');
+define('DIR_DATABASE', 'D:\openserver\OpenServer\domains\opencart.ru/system/database/');
+define('DIR_LANGUAGE', 'D:\openserver\OpenServer\domains\opencart.ru/catalog/language/');
+define('DIR_TEMPLATE', 'D:\openserver\OpenServer\domains\opencart.ru/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\openserver\OpenServer\domains\opencart.ru/system/config/');
+define('DIR_IMAGE', 'D:\openserver\OpenServer\domains\opencart.ru/image/');
+define('DIR_CACHE', 'D:\openserver\OpenServer\domains\opencart.ru/system/cache/');
+define('DIR_DOWNLOAD', 'D:\openserver\OpenServer\domains\opencart.ru/download/');
+define('DIR_LOGS', 'D:\openserver\OpenServer\domains\opencart.ru/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
+?>
